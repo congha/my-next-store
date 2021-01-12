420a8a69c6cc604fcb8487587e8f9db6eef82737
congha <conghc@gmail.com>
2021-01-12 04:35:33 +0000

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,44 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import products from "../products.json";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>Welcome to next store!</h1>
+
+        <ul className={styles.grid}>
+          {products.map((product) => {
+            const { id, title, image, price } = product;
+            return (
+              <li key={id} className={styles.card}>
+                <a>
+                  <img src={image} alt={title} />
+                  <h3>{title}</h3>
+                  <p>{price}</p>
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{" "}
+          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+        </a>
+      </footer>
+    </div>
+  );
+}
